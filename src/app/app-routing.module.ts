f56bea17ea9545324db185b14b3763d05aab58f4
Burkholder <U92AQLF@caremarkrx.net>
2017-10-17 19:46:57 -0500

    bootstrap 4 changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent }          from './components/home/home.component';
 import { DashboardComponent }     from './components/dashboard/dashboard.component';
-import { CommunicationComponent } from './components/communication/communication.component';
+import { CommunicationComponent } from './components/communication/communication.component'
+import { ModalComponent }         from './components/modal/modal.component';
 import { PageNotFoundComponent }  from './components/page-not-found/page-not-found.component';
 import { ProgramComponent }       from './components/program/program.component';
+import { ComponentAsContentComponent } from './components/modal/component-as-content/component-as-content.component';
 
 const routes: Routes = [
   { path: 'home',           component: HomeComponent },
   { path: 'dashboard',      component: DashboardComponent },
   { path: 'communications', component: CommunicationComponent },
   { path: 'program',        component: ProgramComponent },
+  { path: 'modaltest',      component: ModalComponent },
+  { path: 'modalcontenttest',      component: ComponentAsContentComponent },
   { path: '',               redirectTo: '/home', pathMatch: 'full' },
   { path: '**',             component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule }     from '@angular/http';
+import { NgbModule }      from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent }           from './app.component';
+// Basic Page Layout Stuff
+import { NavbarComponent }        from './components/navbar/navbar.component';
+import { FooterComponent }        from './components/footer/footer.component';
 
 import { AppRoutingModule }       from './app-routing.module';
-import { AppComponent }           from './app.component';
-import { CommunicationComponent } from './components/communication/communication.component';
-import { DashboardComponent }     from './components/dashboard/dashboard.component';
-import { FooterComponent }        from './components/footer/footer.component';
+// Application Routing Components
 import { HomeComponent }          from './components/home/home.component';
-import { NavbarComponent }        from './components/navbar/navbar.component';
-import { PageNotFoundComponent }  from './components/page-not-found/page-not-found.component';
+import { DashboardComponent }     from './components/dashboard/dashboard.component';
+import { CommunicationComponent } from './components/communication/communication.component';
 import { ProgramComponent }       from './components/program/program.component';
 
+import { PageNotFoundComponent }  from './components/page-not-found/page-not-found.component';
+
+// Services
 import { CommunicationService }   from './services/communication.service';
 import { ProgramService }         from './services/program.service';
 
@@ -20,25 +26,38 @@ import { ProgramService }         from './services/program.service';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './services/in-memory-data.service';
 
+// Modal stuff
+import { ModalComponent }              from './components/modal/modal.component';
+import { ComponentAsContentComponent } from './components/modal/component-as-content/component-as-content.component';
+import { NgbdModalContent } from './components/modal/component-as-content/component-as-content.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommunicationComponent,
-    DashboardComponent,
-    FooterComponent,
-    HomeComponent,
     NavbarComponent,
+    FooterComponent,
+
+    HomeComponent,
+    DashboardComponent,
+    CommunicationComponent,
+    ProgramComponent,    
+
     PageNotFoundComponent,
-    ProgramComponent
+
+    ModalComponent,
+    ComponentAsContentComponent,
+    NgbdModalContent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
     ,InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
+  entryComponents: [NgbdModalContent],
   providers: [CommunicationService, ProgramService],
   bootstrap: [AppComponent]
 })
