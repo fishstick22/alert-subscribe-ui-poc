a036456a44c6fcd966a185d6fb261287c5f4d9c2
fishstick22 <jeff.burkholder@gmail.com>
2017-10-24 07:48:52 -0500

    JD's suggested ui changes, finally got to save configuratrions correctly

--- src/app/services/program-configuration.service.ts
+++ src/app/services/program-configuration.service.ts
@@ -38,6 +38,25 @@ export class ProgramConfigurationService {
     }
   }
 
+  public async createProgramConfiguration(programConfiguration: ProgramConfiguration): Promise<ProgramConfiguration> {
+    programConfiguration = await this.createProgramConfigurationThruApi(programConfiguration);
+    this.insertProgramConfiguration(programConfiguration);
+    return programConfiguration;
+  }
+
+  private insertProgramConfiguration(programConfiguration: ProgramConfiguration): void {
+    this.programConfigurations.push(programConfiguration);
+  }
+
+  private async createProgramConfigurationThruApi(programConfiguration: ProgramConfiguration): Promise<ProgramConfiguration> {
+    try {
+      const response = await this.http.post(this.progConfigApiEndpoint, JSON.stringify(programConfiguration), {headers: this.headers}).toPromise();
+      return response.json() as ProgramConfiguration;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
